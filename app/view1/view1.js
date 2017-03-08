44970721c90efe9dba737ef646c8b04676b22255
unknown <muthukumars0894@gmail.com>
2017-03-09 02:26:33 +0530

    initial

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,56 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+ .config(['$routeProvider', function($routeProvider) {
+     $routeProvider.when('/view1', {
+     templateUrl: 'view1/view1.html',
+     controller: 'View1Ctrl'
+   });
 }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+.controller('View1Ctrl', ['$scope', function ($scope) {
+//only ment for manager controller
+   $scope.Manager_Model = {  
+                Id: null,  
+                Salary: null,  
+                Name: '',  
+            };  
+  
+            $scope.Manager_List = [];  
+            $scope.AddData = function () {  
+                var _manager = {  
+                    Id: $scope.Manager_List.length + 1,  
+                    Name: $scope.Manager_Model.Name,  
+                    Salary: $scope.Manager_Model.Salary  
+                };  
+                $scope.Manager_List.push(_manager);  
+                 //$scope.ManagerModel = null;//it is used to dis appear the value that you have entered
+                 //this  model affect the bind data function
+            }  
+  
+            $scope.DeleteData = function (manager) {  
+                console.log(manager)
+                var _index = $scope.Manager_List.indexOf(manager);  
+                $scope.Manager_List.splice(_index, 1);  
+            }  
+  
+            $scope.BindSelectedData = function (manager) { 
+              console.log("bind data"); 
+              console.log(manager);
+                $scope.Manager_Model.Id     =   manager.Id;  
+                $scope.Manager_Model.Name   =   manager.Name;  
+                $scope.Manager_Model.Salary =   manager.Salary;  
+            }  
+  
+            $scope.UpdateData =  function () {
+              //console.log("update action")  
+                $.grep($scope.Manager_List, function (e) {  
+                  console.log("update")
+                    if (e.Id == $scope.manager_Model.Id) {  
+                        e.Name = $scope.manager_Model.Name;  
+                        e.Salary = $scope.manager_Model.Salary;  
+                    }  
+                });  
+                
+            }            
+        }]);   
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,61 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+  .controller('View2Ctrl', ['$scope', function($scope) {
+    
+    /* $scope.name = '';
+       $scope.names = [{name:"Chris"}, {name:"Calvin"}];
+  
+      $scope.addName = function() {
+      $scope.names.push( {'name':$scope.name} );
+      $scope.name = '';
+       };*/
+       //"$scope is used for hash declaration of the main function in model"-->denotes the injector
+            $scope.EmpModel = {  
+                Id: null,  
+                Salary: null,  
+                Name: '',  
+            };  
+  
+            $scope.EmpList = [];  
+            $scope.AddData = function () {  
+                var _emp = {  
+                    Id: $scope.EmpList.length + 1,  
+                    Name: $scope.EmpModel.Name,  
+                    Salary: $scope.EmpModel.Salary  
+                };  
+                $scope.EmpList.push(_emp);  
+                 //$scope.EmpModel = null;//it is used to dis appear the value that you have entered
+                 //this emp model affect the bind data function
+            }  
+  
+            $scope.DeleteData = function (emp) {  
+              
+                var _index = $scope.EmpList.indexOf(emp);  
+                $scope.EmpList.splice(_index, 1);  
+            }  
+  
+            $scope.BindSelectedData = function (empl) { 
+              console.log("bind data"); 
+               console.log(empl);
+                $scope.EmpModel.Id = empl.Id;  
+                $scope.EmpModel.Name = empl.Name;  
+                $scope.EmpModel.Salary = empl.Salary;  
+            }  
+  
+            $scope.UpdateData =  function () {
+              console.log("update action")  
+                $.grep($scope.EmpList, function (e) {  
+                  console.log("update")
+                    if (e.Id == $scope.EmpModel.Id) {  
+                        e.Name = $scope.EmpModel.Name;  
+                        e.Salary = $scope.EmpModel.Salary;  
+                    }  
+                });  
+                
+            }  
+  
+            
+        }]);   
+ 
 
-}]);
